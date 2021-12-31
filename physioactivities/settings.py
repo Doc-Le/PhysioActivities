187c4e57bb82c609c5037b63af732bde4b32f943
Leandro De Araujo <leandro.arj@outlook.com>
2021-12-31 11:05:11 +0000

    add domain

--- physioactivities/settings.py
+++ physioactivities/settings.py
@@ -14,7 +14,13 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['physioactivities.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = [
+    'www.physioactivities.com',
+    'physioactivities.com',
+    'physioactivities.herokuapp.com', 
+    'localhost', 
+    '127.0.0.1'
+]
 
 
 # Application definition
