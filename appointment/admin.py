093d3a6a8c4527635a77c18344bb130dfce9d9bd
Leandro De Araujo <leandro.arj@outlook.com>
2021-12-31 00:47:36 +0000

    fix admin site register

--- appointment/admin.py
+++ appointment/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Resource, Appointment, CalendarDate, CalendarTime, Location, ResourceService, Service
 
-# Register your models here.
+
+admin.site.register(Location)
+admin.site.register(Service)
+admin.site.register(Resource)
+admin.site.register(ResourceService)
+admin.site.register(CalendarDate)
+admin.site.register(CalendarTime)
+admin.site.register(Appointment)
