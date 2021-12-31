eec93b25e2bfb31741d850fcce6272d9db277cb9
Leandro De Araujo <leandro.arj@outlook.com>
2021-12-31 01:07:41 +0000

    add signup views

---
+++ signup/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+TEMPLATE_ROOT = 'signup/index.html'
+
+def signup(request):
+    """ Display the user's profile. """
+
+    context = {}
+
+    return render(request, TEMPLATE_ROOT, context)
+
+def login(request):
+    """ Display the user's profile. """
+
+    context = {}
+
+    return render(request, TEMPLATE_ROOT, context)
