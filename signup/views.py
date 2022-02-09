8dd7f124a8740b57f396eb55fbb2e2fbacdef6b4
Leandro De Araujo <leandro.arj@outlook.com>
2022-02-09 13:43:31 +0000

    formatter pep8 views.py

--- signup/views.py
+++ signup/views.py
@@ -2,12 +2,14 @@ from django.shortcuts import render
 
 TEMPLATE_ROOT = 'signup/index.html'
 
+
 def signup(request):
     """ Display the user's profile. """
 
     context = {}
 
     return render(request, TEMPLATE_ROOT, context)
+
 
 def login(request):
     """ Display the user's profile. """
