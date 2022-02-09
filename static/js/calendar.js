52bbe2f3ea13f2380e0a5ae9147f9a29ad45fc80
Leandro De Araujo <leandro.arj@outlook.com>
2022-02-09 13:44:13 +0000

    format calendar.js

--- static/js/calendar.js
+++ static/js/calendar.js
@@ -1,9 +1,7 @@
-function yearleepy(year)
-{
-  return ((year % 4 == 0) && ((year % 100 != 0) || (year % 400 == 0)));
+function yearleepy(year) {
+  return year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
 }
-function printCalendar()
-{
+function printCalendar() {
   data = new Date();
 
   let year = data.getYear();
@@ -16,46 +14,58 @@ function printCalendar()
   const tempDate = new Date(year, month - 1, 1);
   const firstDayMonth = tempDate.getDay();
 
-  if(weekDay == 0) weekDay = 7;
-  if(firstDayMonth == 0) firstDayMonth = 7;
+  if (weekDay == 0) weekDay = 7;
+  if (firstDayMonth == 0) firstDayMonth = 7;
 
-  switch(month){
-    case 1 : monthName = "January";
-             daysInMonth = 31;
-             break;
-    case 2 : monthName = "February";
-             daysInMonth = yearleepy(year)?29:28;
-             break;
-    case 3 : monthName = "March";
-             daysInMonth = 31;
-             break;
-    case 4 : monthName = "April";
-             daysInMonth = 30;
-             break;
-    case 5 : monthName = "May";
-             daysInMonth = 31;
-             break;
-    case 6 : monthName = "June";
-             daysInMonth = 30;
-             break;
-    case 7 : monthName = "July";
-             daysInMonth = 31;
-             break;
-    case 8 : monthName = "August";
-             daysInMonth = 31;
-             break;
-    case 9 : monthName = "September";
-             daysInMonth = 30;
-             break;
-    case 10 : monthName = "October";
-             daysInMonth = 31;
-             break;
-    case 11 : monthName = "November";
-             daysInMonth = 30;
-             break;
-    case 12 : monthName = "December";
-             daysInMonth = 31;
-             break;
+  switch (month) {
+    case 1:
+      monthName = "January";
+      daysInMonth = 31;
+      break;
+    case 2:
+      monthName = "February";
+      daysInMonth = yearleepy(year) ? 29 : 28;
+      break;
+    case 3:
+      monthName = "March";
+      daysInMonth = 31;
+      break;
+    case 4:
+      monthName = "April";
+      daysInMonth = 30;
+      break;
+    case 5:
+      monthName = "May";
+      daysInMonth = 31;
+      break;
+    case 6:
+      monthName = "June";
+      daysInMonth = 30;
+      break;
+    case 7:
+      monthName = "July";
+      daysInMonth = 31;
+      break;
+    case 8:
+      monthName = "August";
+      daysInMonth = 31;
+      break;
+    case 9:
+      monthName = "September";
+      daysInMonth = 30;
+      break;
+    case 10:
+      monthName = "October";
+      daysInMonth = 31;
+      break;
+    case 11:
+      monthName = "November";
+      daysInMonth = 30;
+      break;
+    case 12:
+      monthName = "December";
+      daysInMonth = 31;
+      break;
   }
 
   document.write("<TABLE border = 1><TR>");
@@ -75,18 +85,17 @@ function printCalendar()
 
   var j = daysInMonth + firstDayMonth - 1;
 
-  for(var i = 0; i < j; i++){
-    if(i < firstDayMonth - 1){
+  for (var i = 0; i < j; i++) {
+    if (i < firstDayMonth - 1) {
       document.write("<TD bgcolor='white'></TD>");
       continue;
     }
-    if((i % 7) == 0){
+    if (i % 7 == 0) {
       document.write("</TR><TR>");
     }
-    if((i - firstDayMonth + 2) == monthDay){
+    if (i - firstDayMonth + 2 == monthDay) {
       color = "#a5c422";
-    }
-    else{
+    } else {
       color = "#f9f9f9";
     }
     document.write("<TD bgcolor='" + color + "' align='center'>");
