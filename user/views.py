d3ba877eef691c94d2fbf7a37335a06173caa632
Leandro De Araujo <leandro.arj@outlook.com>
2022-02-10 22:37:20 +0000

    created required login_url views.py

--- user/views.py
+++ user/views.py
@@ -67,4 +67,25 @@ def signup_form(request):
     return render(request, 'signup_form.html', context)
 
 
+@login_required(login_url='/login')
+def user_update(request):
+    if request.method == 'POST':
+        user_form = UserUpdateForm(request.POST, instance=request.user)
+        profile_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.userprofile)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, 'Your account has been updated!')
+            return HttpResponseRedirect('/user')
+    else:
+        user_form = UserUpdateForm(instance=request.user)
+        profile_form = ProfileUpdateForm(instance=request.user.userprofile)
+        context = {
+            'user_form': user_form,
+            'profile_form': profile_form
+        }
+        return render(request, 'user_update.html', context)
+
+
+
 
