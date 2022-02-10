521c1e6bc352e3ac7d1ba8288d6d89a4ccb3bde9
Leandro De Araujo <leandro.arj@outlook.com>
2022-02-10 22:05:46 +0000

    created UserProfile models.py

--- user/models.py
+++ user/models.py
@@ -1,3 +1,8 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone = models.CharField(blank=True, max_length=20)
+
+  
