eaeddef368a428db29f9e728b61e2e9dc56ea7d2
Leandro De Araujo <leandro.arj@outlook.com>
2022-02-10 22:10:27 +0000

    formatted models.py

--- user/models.py
+++ user/models.py
@@ -1,13 +1,14 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     phone = models.CharField(blank=True, max_length=20)
 
- def __str__(self):
+  def __str__(self):
         return self.user.username
 
- def user_name(self):
+  def user_name(self):
         return self.user.first_name + ' ' + self.user.last_name + ' [' + self.user.username + '] '
   
