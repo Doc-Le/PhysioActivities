7f6f10d9134c81e0789f35bda297e1a4d5ff0ee0
Leandro De Araujo <leandro.arj@outlook.com>
2022-02-10 22:07:02 +0000

    added def_str_user_name models.py

--- user/models.py
+++ user/models.py
@@ -5,4 +5,9 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     phone = models.CharField(blank=True, max_length=20)
 
+ def __str__(self):
+        return self.user.username
+
+ def user_name(self):
+        return self.user.first_name + ' ' + self.user.last_name + ' [' + self.user.username + '] '
   
