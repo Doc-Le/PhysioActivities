c253e32b1f7ff4f636a641e56ac2a9509f150338
Leandro De Araujo <leandro.arj@outlook.com>
2022-02-10 21:37:59 +0000

    added userProfile.objects.get

--- home/views.py
+++ home/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from user.models import UserProfile
 
 def index(request):
     """ A view to return the index page """
-
-    return render(request, 'home/index.html')
+    current_user = request.user
+    try:
+        profile = UserProfile.objects.get(user_id=current_user.id)
+    except UserProfile.DoesNotExist:
+        profile = None
+    context = {'profile': profile}
+    return render(request, 'home/index.html', context)
