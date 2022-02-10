dbfe6b796504219c2a6999b94cae27c466c5c8fa
Leandro De Araujo <leandro.arj@outlook.com>
2022-02-10 21:55:33 +0000

    deleted select and input

--- user/forms.py
+++ user/forms.py
@@ -1,47 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.contrib.auth.models import User
-from django.forms import TextInput, EmailInput, Select, FileInput
-
+from django.forms import TextInput, EmailInput
 from user.models import UserProfile
 
 
-class SignUpForm(UserCreationForm):
-    username = forms.CharField(max_length=30,label= 'User Name :')
-    email = forms.EmailField(max_length=200,label= 'Email :')
-    first_name = forms.CharField(max_length=100, help_text='First Name',label= 'First Name :')
-    last_name = forms.CharField(max_length=100, help_text='Last Name',label= 'First Name :')
-    phone = forms.phoneField(max_length=200,label= 'Phone :')
-    location = forms.locationField(max_length=200,label= 'Location :')
-    
-    class Meta:
-        model = User
-        fields = ('username', 'email','first_name','last_name', 'password1', 'password2', )
-
-class UserUpdateForm(UserChangeForm):
-    class Meta:
-        model = User
-        fields = ( 'username','email','first_name','last_name')
-        widgets = {
-            'username'  : TextInput(attrs={'class': 'input','placeholder':'username'}),
-            'email'     : EmailInput(attrs={'class': 'input','placeholder':'email'}),
-            'first_name': TextInput(attrs={'class': 'input','placeholder':'first_name'}),
-            'last_name' : TextInput(attrs={'class': 'input','placeholder':'last_name' }),
-        }
-
-CITY = [
-    ('Dublin', 'Wicklow town'),
-    ('Arklow', 'Gorey'),
-    ('', ''),
-]
-class ProfileUpdateForm(forms.ModelForm):
-    class Meta:
-        model = UserProfile
-        fields = ('phone', 'address', 'city','country','image','language')
-        widgets = {
-            'phone'     : TextInput(attrs={'class': 'input','placeholder':'phone'}),
-            'address'   : TextInput(attrs={'class': 'input','placeholder':'address'}),
-            'city'      : Select(attrs={'class': 'input','placeholder':'city'},choices=CITY),
-            'country'   : TextInput(attrs={'class': 'input','placeholder':'country' }),
-            'image'     : FileInput(attrs={'class': 'input', 'placeholder': 'image', }),
-        }
