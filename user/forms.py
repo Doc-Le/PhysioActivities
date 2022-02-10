271b52fe1b347d232071c06b94a93805c985d7c2
Leandro De Araujo <leandro.arj@outlook.com>
2022-02-10 17:08:00 +0000

    create class meta user

---
+++ user/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.models import User
+from django.forms import TextInput, EmailInput, Select, FileInput
+
+from user.models import UserProfile
+
+
+class SignUpForm(UserCreationForm):
+    username = forms.CharField(max_length=30,label= 'User Name :')
+    email = forms.EmailField(max_length=200,label= 'Email :')
+    first_name = forms.CharField(max_length=100, help_text='First Name',label= 'First Name :')
+    last_name = forms.CharField(max_length=100, help_text='Last Name',label= 'First Name :')
+    phone = forms.phoneField(max_length=200,label= 'Phone :')
+    location = forms.locationField(max_length=200,label= 'Location :')
+    
+    
