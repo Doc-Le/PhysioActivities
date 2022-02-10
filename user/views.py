a72bcd7988d1027b0baaf1f01413b79418f3bdb2
Leandro De Araujo <leandro.arj@outlook.com>
2022-02-10 22:17:41 +0000

    added from user.forms import

--- user/views.py
+++ user/views.py
@@ -1,3 +1,11 @@
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import PasswordChangeForm
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
-# Create your views here.
+from user.forms import SignUpForm, UserUpdateForm, ProfileUpdateForm
+from user.models import UserProfile
+
+
