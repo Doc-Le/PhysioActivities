5ef71c91ff23c20d908ae56a69cb5eb6cea1f544
Leandro De Araujo <leandro.arj@outlook.com>
2022-02-10 22:12:03 +0000

    created urlpatterns urls.py

---
+++ user/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='user_index'),
+    path('update/', views.user_update, name='user_update'),
+    path('password/', views.user_password, name='user_password'),
+]
