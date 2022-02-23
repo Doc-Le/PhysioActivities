ea11e2e4af1bd6afbda900f00288f68dedae45c8
Leandro De Araujo <leandro.arj@outlook.com>
2022-02-23 22:17:12 +0000

    Added  class meta in models.py

--- products/models.py
+++ products/models.py
@@ -5,6 +5,10 @@ from django.db import models
 
 
 class Category(models.Model):
+    
+    class Meta:
+        verbose_name_plural = 'Categories'
+    
     name = models.CharField(max_length=254)
     
     def __str__(self):
