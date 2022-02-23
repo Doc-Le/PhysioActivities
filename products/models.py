543fa46f89f25eb665ae1cecf2a415a4cd9dec83
Leandro De Araujo <leandro.arj@outlook.com>
2022-02-23 21:46:28 +0000

    added class category / products in models.py

--- products/models.py
+++ products/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=254)
+    
+    def __str__(self):
+        return self.name
+
+   
+class Product(models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    name = models.CharField(max_length=254)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    
+
+    def __str__(self):
+        return self.name
