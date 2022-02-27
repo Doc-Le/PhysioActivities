7821924c9e85c7bea4722814ae4ef72e345a0978
Leandro De Araujo <leandro.arj@outlook.com>
2022-02-27 22:39:27 +0000

    created models .py

---
+++ services/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+
+class clinicians(models.Model):
+
+    
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class services(models.Model):
+    clinicians = models.ForeignKey('Clinician', null=True, blank=True, on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    
+    def __str__(self):
+        return self.name
