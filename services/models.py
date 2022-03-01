587e5bea9824841af4f265e1d5a808da87f5ac6a
Leandro De Araujo <leandro.arj@outlook.com>
2022-03-01 10:49:46 +0000

    added clinician details in models.py

--- services/models.py
+++ services/models.py
@@ -1,25 +1,27 @@
 from django.db import models
 
 
-class clinicians(models.Model):
+class Clinician(models.Model):
+    full_name = models.CharField(max_length=100)
+    gender = models.CharField(max_length=10)
 
-    
-    name = models.CharField(max_length=254)
-    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+    class Meta: 
+        ordering = ['full_name']
 
     def __str__(self):
-        return self.name
+        return self.full_name
 
-    def get_friendly_name(self):
-        return self.friendly_name
-
-
-class services(models.Model):
-    clinicians = models.ForeignKey('Clinician', null=True, blank=True, on_delete=models.SET_NULL)
-    sku = models.CharField(max_length=254, null=True, blank=True)
+class Service(models.Model):
+    clinicians = models.ManyToManyField(Clinician)
     name = models.CharField(max_length=254)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     
+    class Meta: 
+        ordering = ['name']
+
     def __str__(self):
         return self.name
+
+    def get_display_price(self):
+        return "{0:.2f}".format(self.price / 100)
