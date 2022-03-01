6cb57e52fc20021a18c8e39957bd07eb86c06fb2
Leandro De Araujo <leandro.arj@outlook.com>
2022-03-01 09:50:34 +0000

    creating @login_required

--- bookings/views.py
+++ bookings/views.py
@@ -1,25 +1,24 @@
+from re import template
 from django.shortcuts import render, redirect, reverse
 
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.conf import settings
-from .models import Order, OrderLineItem
-from appointment.models import Appointment
 
 from .forms import OrderForm
 
-
-def checkout(request):
-    
-    Appointment = request.session.get('appointment', {})
-    if not Appointment:
-        messages.error(request, "There is no appoiment schedules")
-        return redirect(reverse('appointment'))
+@login_required(login_url='/login?show_signup=true')
+def bookings(request):    
+    # Booking = request.session.get('booking', {})
+    # if not Booking:
+    #     messages.error(request, "There is no appoiment schedules")
+    #     return redirect(reverse('bookings'))
 
     order_form = OrderForm()
-    template = 'checkout/checkout.html'
+    template = 'bookings/bookings.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': "settings.STRIPE_PUBLIC_KEY",
+        'stripe_public_key': settings.STRIPE_PUBLIC_KEY,
         'client_secret': 'test client secret',
     }
 
