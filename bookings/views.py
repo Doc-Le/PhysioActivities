17be1806d6b48e9c6ff24acb813891167c7797b5
Leandro De Araujo <leandro.arj@outlook.com>
2022-03-02 22:11:29 +0000

    added from .forms import bookingForm in views.py

--- bookings/views.py
+++ bookings/views.py
@@ -5,19 +5,20 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.conf import settings
 
-from .forms import OrderForm
+from .forms import BookingForm
+
 
 @login_required(login_url='/login?show_signup=true')
-def bookings(request):    
+def bookings(request):
     # Booking = request.session.get('booking', {})
     # if not Booking:
     #     messages.error(request, "There is no appoiment schedules")
     #     return redirect(reverse('bookings'))
 
-    order_form = OrderForm()
+    form = BookingForm()
     template = 'bookings/bookings.html'
     context = {
-        'order_form': order_form,
+        'form': form,
         'stripe_public_key': settings.STRIPE_PUBLIC_KEY,
         'client_secret': 'test client secret',
     }
