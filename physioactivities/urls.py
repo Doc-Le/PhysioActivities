2d720d6fca4e91fee482465256cbd7bc3220ff08
Leandro De Araujo <leandro.arj@outlook.com>
2022-03-02 22:13:31 +0000

    deleted appointments.views in urls.py

--- physioactivities/urls.py
+++ physioactivities/urls.py
@@ -2,16 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from appointment.views import (
-    SuccessView,
-    CancelView,
-    AppointmentView,
-    CreateCheckoutSessionView,
-    StripeIntentView,
-    # get_services,
-    get_times,
-    stripe_webhook
-)
 from user import views as UserViews
 
 urlpatterns = [
