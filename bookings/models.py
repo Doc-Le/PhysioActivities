0c94f650dd1219c254161ab29acee07168b7264e
Leandro De Araujo <leandro.arj@outlook.com>
2022-03-02 22:10:22 +0000

    added bookingmodels.models details in  models.py

--- bookings/models.py
+++ bookings/models.py
@@ -1,56 +1,36 @@
 import uuid
 
 from django.db import models
-from django.db.models import Sum
 from services.models import Clinician, Service
+from user.models import User
 
-class Order(models.Model):
-    order_number = models.CharField(max_length=32, null=False, editable=False)
-    full_name = models.CharField(max_length=50, null=False, blank=False)
-    email = models.EmailField(max_length=254, null=False, blank=False)
-    phone_number = models.CharField(max_length=20, null=False, blank=False)
-    country = models.CharField(max_length=40, null=False, blank=False)
-    postcode = models.CharField(max_length=20, null=True, blank=True)
-    town_or_city = models.CharField(max_length=40, null=False, blank=False)
-    street_address1 = models.CharField(max_length=80, null=False, blank=False)
-    street_address2 = models.CharField(max_length=80, null=True, blank=True)
-    county = models.CharField(max_length=80, null=True, blank=True)
-    date = models.DateTimeField(auto_now_add=True)
-    delivery_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
-    order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
-    grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
-
-    def _generate_order_number(self):
-        """
-        Generate a random, unique order number using UUID
-        """
-        return uuid.uuid4().hex.upper()
+class Booking(models.Model):
+    booking_number = models.CharField(max_length=32, null=False, editable=False)
+    user = models.ForeignKey(User, null=False, blank=False, on_delete=models.CASCADE)
+    service = models.ForeignKey(Service, null=False, blank=False, on_delete=models.CASCADE)
+    clinician = models.ForeignKey(Clinician, null=False, blank=False, on_delete=models.CASCADE)
+    #  date time field:
+    #  > weekends
+    #  > time from 00:00 to 09:00 and 18:00 to 23:00
+    #  > time > minute 00 and 30 
+    datetime = models.DateTimeField()
+    total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False)
 
     def save(self, *args, **kwargs):
         """
         Override the original save method to set the order number
         if it hasn't been set already.
         """
-        if not self.order_number:
-            self.order_number = self._generate_order_number()
-        super().save(*args, **kwargs)
-
-    def __str__(self):
-        return self.order_number
-
-class Booking(models.Model):
-    datetime = models.DateTimeField()
-    order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='order')
-    service = models.ForeignKey(Service, null=False, blank=False, on_delete=models.CASCADE)
-    clinician = models.ForeignKey(Clinician, null=False, blank=False, on_delete=models.CASCADE)
-    total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
-    def save(self, *args, **kwargs):
-        """
-        Override the original save method to set the booking total
-        and update the order total.
-        """
+        if not self.booking_number:
+            self.booking_number = self._generate_booking_number()
         self.total = self.service.price
         super().save(*args, **kwargs)
 
+    def _generate_booking_number(self):
+        """
+        Generate a random, unique booking number using UUID
+        """
+        return uuid.uuid4().hex.upper()
+
     def __str__(self):
-        return f'Service {self.service.name} on order {self.order.order_number}'
+        return self.booking_number
