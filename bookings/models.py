f134037ea819050464ba5b2f9a49a1ab8c8b45e9
Leandro De Araujo <leandro.arj@outlook.com>
2022-03-08 19:59:52 +0000

    fixed class Bookingdate in models.py

--- bookings/models.py
+++ bookings/models.py
@@ -1,18 +1,29 @@
+from cProfile import label
 import uuid
-
+from datetime import datetime
 from django.db import models
 from services.models import Clinician, Service
 from user.models import User
+
+class BookingDate(models.Model):
+    date = models.DateField(auto_now=False, auto_now_add=False)
+
+    def __str__(self):
+        return self.date.strftime("%d/%m/%Y")
+
+class BookingTime(models.Model):
+    time = models.TimeField()
+   
+    def __str__(self):
+        return self.time.strftime("%H:%M")
 
 class Booking(models.Model):
     booking_number = models.CharField(max_length=32, null=False, editable=False)
     user = models.ForeignKey(User, null=False, blank=False, on_delete=models.CASCADE)
     service = models.ForeignKey(Service, null=False, blank=False, on_delete=models.CASCADE)
-    clinician = models.ForeignKey(Clinician, null=False, blank=False, on_delete=models.CASCADE)
-    # find out how to exclude from date time field:
-    #  > weekends
-    #  > time from 00:00 to 09:00 and 18:00 to 23:00
-    #  > time > minute 00 and 30 
+    clinician = models.ForeignKey(Clinician, null=False, blank=False, on_delete=models.CASCADE) 
+    date = models.ForeignKey(BookingDate, null=False, blank=False, on_delete=models.CASCADE)
+    time = models.ForeignKey(BookingTime, null=False, blank=False, on_delete=models.CASCADE)
     datetime = models.DateTimeField()
     total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False)
 
@@ -23,6 +34,8 @@ class Booking(models.Model):
         """
         if not self.booking_number:
             self.booking_number = self._generate_booking_number()
+        
+        self.datetime = datetime.combine(self.date, self.time)
         self.total = self.service.price
         super().save(*args, **kwargs)
 
