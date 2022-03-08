bdf1cef9e2e5a1e66028c85db6a88f1f39de7dbd
Leandro De Araujo <leandro.arj@outlook.com>
2022-03-08 20:06:48 +0000

    fixed urlpatterns

--- services/urls.py
+++ services/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.all_services, name='services')
+    path('', views.all_services, name='services'),
+    path('<int:service_id>/', views.get_service, name='get_service'),
 ]
