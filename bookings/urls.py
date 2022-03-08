b7264b54c45cd08b014fd9cc7730a767f37f8ae2
Leandro De Araujo <leandro.arj@outlook.com>
2022-03-08 20:01:05 +0000

    added urlsatterns in urls.py

--- bookings/urls.py
+++ bookings/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.bookings, name='bookings')
+    path('', views.bookings, name='bookings'),
+    path('dates/', views.get_dates, name='get_dates'),
+    path('times/<int:date_id>/', views.get_times, name='get_times'),
 ]
