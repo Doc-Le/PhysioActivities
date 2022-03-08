2f1245b93b6179feafca540d4dcec6200386147a
Leandro De Araujo <leandro.arj@outlook.com>
2022-03-08 19:58:13 +0000

    added bookings date _ time fields

--- bookings/admin.py
+++ bookings/admin.py
@@ -1,16 +1,28 @@
 from django.contrib import admin
-from .models import Booking
+from .models import Booking, BookingDate, BookingTime
 
 
 class BookingAdmin(admin.TabularInline):
     model = Booking
 
-    fields = ('datetime', 'user', 'service', 'clinician', 'total',)
+    fields = ('user', 'service', 'clinician', 'date', 'time', 'total',)
     
-    readonly_fields = ('total',)
+    readonly_fields = ('datetime', 'total',)
     
     list_display = ('datetime', 'user', 'service', 'clinician', 'total',)
 
     ordering = ('-datetime',)
 
+class BookingDateAdmin(admin.TabularInline):
+    model = BookingDate
+    fields = ('date',)
+    ordering = ('-date',)
+
+class BookingTimeAdmin(admin.TabularInline):
+    model = BookingTime
+    fields = ('time',)
+    ordering = ('-time',)
+
 admin.site.register(Booking)
+admin.site.register(BookingDate)
+admin.site.register(BookingTime)
