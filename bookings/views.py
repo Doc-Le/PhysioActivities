db8b3c5187c84d60b8a4e4c599da299a7b8e6c5f
Leandro De Araujo <leandro.arj@outlook.com>
2022-03-09 19:41:36 +0000

    added from models import booking

--- bookings/views.py
+++ bookings/views.py
@@ -10,31 +10,21 @@ from django.http import JsonResponse, HttpResponse
 from django.urls import reverse
 from django.views.decorators.http import require_POST
 
-from .forms import BookingForm
-from .models import Booking, BookingDate, BookingTime
+from .models import Booking
 
 import stripe
 import json
 
 @login_required(login_url='/login?show_signup=true')
 def bookings(request):    
-    form = BookingForm()
 
+    bag = request.session.get('bag', {})
     user = request.user
 
-    # print({
-    #     'username': str(user),
-    #     'id': user.id,
-    #     'first_name': user.first_name,
-    #     'last_name': user.last_name,
-    #     'full_name': user.get_full_name(),
-    #     'email': user.email,
-    # })
-    
     template = 'bookings/bookings.html'
     context = {
         'user': user,
-        'form': form,
+        'bag': bag,
         'stripe_public_key': settings.STRIPE_PUBLIC_KEY,
         'client_secret': 'test client secret',
     }
@@ -179,56 +169,3 @@ def bookings(request):
 #     }
 
 #     return render(request, template, context)
-
-
-def get_dates(request):
-    dates = get_available_dates()
-    
-    return JsonResponse({
-        'data': dates
-    })
-
-def get_times(request, date_id: int):
-    times = get_available_times(date_id)
-    
-    return JsonResponse({
-        'data': times
-    })
-
-def has_time_available(date_id: int, time_id: int):
-    date = BookingDate.objects.get(id=date_id)
-    time = BookingTime.objects.get(id=time_id)
-    any = Booking.objects.filter(date=date,time=time).count()
-    if any > 0:
-        return False
-    else:
-        return True
-
-def get_available_times(date_id: int):
-    bookingTimes = BookingTime.objects.all()
-    data = []
-    if date_id is None:
-        return data
-        
-    for time in bookingTimes:
-        if has_time_available(date_id, time.id):
-            data.append({
-                'id': time.id,
-                'time': str(time)
-            })
-
-    return data
-
-def get_available_dates():
-    now = datetime.now()
-    bookingDates = BookingDate.objects.filter(Q(date__gt=now.date()))
-    data = []
-    for date in bookingDates:
-        any = get_available_times(date.id)
-        if len(any) > 0:
-            data.append({
-                'id': date.id,
-                'date': str(date)
-            })
-
-    return data
