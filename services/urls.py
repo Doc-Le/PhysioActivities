e9063e16d255e0ffd0e1d31bedcd679425d5c620
Leandro De Araujo <leandro.arj@outlook.com>
2022-03-09 19:33:50 +0000

    created urlpatterns date&times

--- services/urls.py
+++ services/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.all_services, name='services'),
+    path('', views.services, name='services'),
+    path('all/', views.all_services, name='all_services'),
     path('<int:service_id>/', views.get_service, name='get_service'),
+    path('dates/', views.get_dates, name='get_dates'),
+    path('times/<int:date_id>/', views.get_times, name='get_times'),
 ]
