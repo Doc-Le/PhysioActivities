49c49507a8aac256ebdccf1644fa1a1887c4cf83
Leandro De Araujo <leandro.arj@outlook.com>
2022-03-09 19:38:56 +0000

    deleted class booking details

--- bookings/models.py
+++ bookings/models.py
@@ -1,29 +1,16 @@
-from cProfile import label
 import uuid
 from datetime import datetime
 from django.db import models
-from services.models import Clinician, Service
+from services.models import Clinician, Service, ServiceDate, ServiceTime
 from user.models import User
-
-class BookingDate(models.Model):
-    date = models.DateField(auto_now=False, auto_now_add=False)
-
-    def __str__(self):
-        return self.date.strftime("%d/%m/%Y")
-
-class BookingTime(models.Model):
-    time = models.TimeField()
-   
-    def __str__(self):
-        return self.time.strftime("%H:%M")
 
 class Booking(models.Model):
     booking_number = models.CharField(max_length=32, null=False, editable=False)
     user = models.ForeignKey(User, null=False, blank=False, on_delete=models.CASCADE)
     service = models.ForeignKey(Service, null=False, blank=False, on_delete=models.CASCADE)
     clinician = models.ForeignKey(Clinician, null=False, blank=False, on_delete=models.CASCADE) 
-    date = models.ForeignKey(BookingDate, null=False, blank=False, on_delete=models.CASCADE)
-    time = models.ForeignKey(BookingTime, null=False, blank=False, on_delete=models.CASCADE)
+    date = models.ForeignKey(ServiceDate, null=False, blank=False, on_delete=models.CASCADE)
+    time = models.ForeignKey(ServiceTime, null=False, blank=False, on_delete=models.CASCADE)
     datetime = models.DateTimeField()
     total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False)
 
