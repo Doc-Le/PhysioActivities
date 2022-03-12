e5f479ac6865d0b61650ad7579f2209c5fe7a252
Leandro De Araujo <leandro.arj@outlook.com>
2022-03-12 21:43:07 +0000

    no change in urls.py

--- bookings/urls.py
+++ bookings/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.bookings, name='bookings'),
-    # path('cancel/', views.as_view(), name='cancel'),
-    # path('success/', views.as_view(), name='success'),
+    #path('cancel/', views.bookings(), name='cancel'),
+    #path('bookings_success/', views.bookings_success(), name='success'),
 ]
