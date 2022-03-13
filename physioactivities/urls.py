854a62aadbd893a03c378cc1aa9153c6a265de53
Leandro De Araujo <leandro.arj@outlook.com>
2022-03-13 23:29:05 +0000

    delete path comments

--- physioactivities/urls.py
+++ physioactivities/urls.py
@@ -5,21 +5,13 @@ from django.conf.urls.static import static
 from user import views as UserViews
 
 urlpatterns = [
+    path('', include('home.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('user/', include('user.urls'), name='user'),
     path('login/', UserViews.login_form, name='login'),
     path('logout/', UserViews.logout_func, name='logout'),
     path('signup/', UserViews.signup_form, name='signup'),
-    path('', include('home.urls')),
-    # path('webhooks/stripe/', stripe_webhook, name='stripe-webhook'),
-    # path('appointment/', AppointmentView.as_view(), name='appointment'),
-    # path('cancel/', CancelView.as_view(), name='cancel'),
-    # path('success/', SuccessView.as_view(), name='success'),
-    # path('create-checkout-session/<pk>/', CreateCheckoutSessionView.as_view(), name='create-checkout-session'),
-    # path('create-payment-intent/<pk>/', StripeIntentView.as_view(), name='create-payment-intent'),
-    # path('services/<int:resource_id>/', get_services, name='get_services'),
-    # path('times/<int:date_id>/', get_times, name='get_times'),
     path('services/', include('services.urls'), name='services'),
     path('bookings/', include('bookings.urls'), name='bookings'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
