57d9bd8bfc219c60f8ec1e51576ef3c3dccdcf95
Doc-Le <76841050+Doc-Le@users.noreply.github.com>
2022-05-05 21:15:50 +0100

    Format stripe_elements.js

--- bookings/static/bookings/js/stripe_elements.js
+++ bookings/static/bookings/js/stripe_elements.js
@@ -4,52 +4,54 @@
     CSS from here: 
     https://stripe.com/docs/stripe-js
 */
-var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
-var clientSecret = $('#id_client_secret').text().slice(1, -1);
+var stripePublicKey = $("#id_stripe_public_key").text().slice(1, -1);
+var clientSecret = $("#id_client_secret").text().slice(1, -1);
 var stripe = Stripe(stripePublicKey);
 var elements = stripe.elements();
 var style = {
-    base: {
-        color: '#000',
-        fontFamily: '"Poppins", sans-serif',
-        fontSmoothing: 'antialiased',
-        fontSize: '14px',
-        '::placeholder': {
-            color: '#aab7c4'
-        }
+  base: {
+    color: "#000",
+    fontFamily: '"Poppins", sans-serif',
+    fontSmoothing: "antialiased",
+    fontSize: "14px",
+    "::placeholder": {
+      color: "#aab7c4",
     },
-    invalid: {
-        color: '#dc3545',
-        iconColor: '#dc3545'
-    }
+  },
+  invalid: {
+    color: "#dc3545",
+    iconColor: "#dc3545",
+  },
 };
-var card = elements.create('card', {style: style});
-card.mount('#card-element');
+var card = elements.create("card", { style: style });
+card.mount("#card-element");
 
 // Handle form submit
-var form = document.getElementById('booking-form');
+var form = document.getElementById("booking-form");
 
-form.addEventListener('submit', function (ev) {
-    ev.preventDefault();
-    card.update({ 'disabled': true});
-    $('#complete-payment-button').attr('disabled', true);
-    stripe.confirmCardPayment(clientSecret, { 
-        payment_method: {
-            card: card,
-        }
-    }).then(function(result) {
-        if (result.error) {
-            var errorDiv = document.getElementById('card-errors');
-            var html = `
+form.addEventListener("submit", function (ev) {
+  ev.preventDefault();
+  card.update({ disabled: true });
+  $("#complete-payment-button").attr("disabled", true);
+  stripe
+    .confirmCardPayment(clientSecret, {
+      payment_method: {
+        card: card,
+      },
+    })
+    .then(function (result) {
+      if (result.error) {
+        var errorDiv = document.getElementById("card-errors");
+        var html = `
                 <span class="icon" role="alert">
                 <i class="fas fa-times"></i>
                 </span>
                 <span>${result.error.message}</span>`;
-            $(errorDiv).html(html);
-            card.update({ 'disabled': false});
-            $('#complete-payment-button').attr('disabled', false);
-        } else if (result.paymentIntent.status === 'succeeded') {
-            form.submit();
-        }
+        $(errorDiv).html(html);
+        card.update({ disabled: false });
+        $("#complete-payment-button").attr("disabled", false);
+      } else if (result.paymentIntent.status === "succeeded") {
+        form.submit();
+      }
     });
 });
