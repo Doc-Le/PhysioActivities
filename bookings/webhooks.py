1d66c9d8b77bb680bd4279c33c649e29ac1f89eb
Doc-Le <76841050+Doc-Le@users.noreply.github.com>
2022-05-06 21:29:05 +0100

    Uncomment webhook_handler.py

--- bookings/webhooks.py
+++ bookings/webhooks.py
@@ -1,55 +1,56 @@
-# from django.conf import settings
-# from django.http import HttpResponse
-# from django.views.decorators.http import require_POST
-# from django.views.decorators.csrf import csrf_exempt
+  from django.conf import settings
+  from django.http import HttpResponse
+  from django.views.decorators.http import require_POST
+  from django.views.decorators.csrf import csrf_exempt
 
-# from .webhook_handler import StripeWH_Handler
+  from .webhook_handler import StripeWH_Handler
 
-# import stripe
+  import stripe
 
 
-# @require_POST
-# @csrf_exempt
-# def webhook(request):
-#     """Listen for webhooks from Stripe"""
-#     # Setup
-#     wh_secret = settings.STRIPE_WH_SECRET
-#     stripe.api_key = settings.STRIPE_SECRET_KEY
+@require_POST
+@csrf_exempt
 
-#     # Get the webhook data and verify its signature
-#     payload = request.body
-#     sig_header = request.META['HTTP_STRIPE_SIGNATURE']
-#     event = None
+def webhook(request):
+    """Listen for webhooks from Stripe"""
+    # Setup
+    wh_secret = settings.STRIPE_WH_SECRET
+    stripe.api_key = settings.STRIPE_SECRET_KEY
 
-#     try:
-#         event = stripe.Webhook.construct_event(
-#             payload, sig_header, wh_secret
-#         )
-#     except ValueError as e:
-#         # Invalid payload
-#         return HttpResponse(content=e, status=400)
-#     except stripe.error.SignatureVerificationError as e:
-#         # Invalid signature
-#         return HttpResponse(content=e, status=400)
-#     except Exception as e:
-#         return HttpResponse(content=e, status=400)
+    # Get the webhook data and verify its signature
+    payload = request.body
+    sig_header = request.META['HTTP_STRIPE_SIGNATURE']
+    event = None
 
-#     # Set up a webhook handler
-#     handler = StripeWH_Handler(request)
+    try:
+        event = stripe.Webhook.construct_event(
+            payload, sig_header, wh_secret
+        )
+    except ValueError as e:
+        # Invalid payload
+        return HttpResponse(content=e, status=400)
+    except stripe.error.SignatureVerificationError as e:
+        # Invalid signature
+        return HttpResponse(content=e, status=400)
+    except Exception as e:
+        return HttpResponse(content=e, status=400)
 
-#     # Map webhook events to relevant handler functions
-#     event_map = {
-#         'payment_intent.succeeded': handler.handle_payment_intent_succeeded,
-#         'payment_intent.payment_failed': handler.handle_payment_intent_payment_failed,
-#     }
+    # Set up a webhook handler
+    handler = StripeWH_Handler(request)
 
-#     # Get the webhook type from Stripe
-#     event_type = event['type']
+    # Map webhook events to relevant handler functions
+    event_map = {
+        'payment_intent.succeeded': handler.handle_payment_intent_succeeded,
+        'payment_intent.payment_failed': handler.handle_payment_intent_payment_failed,
+    }
 
-#     # If there's a handler for it, get it from the event map
-#     # Use the generic one by default
-#     event_handler = event_map.get(event_type, handler.handle_event)
+    # Get the webhook type from Stripe
+    event_type = event['type']
 
-#     # Call the event handler with the event
-#     response = event_handler(event)
-#     return response
+    # If there's a handler for it, get it from the event map
+    # Use the generic one by default
+    event_handler = event_map.get(event_type, handler.handle_event)
+
+    # Call the event handler with the event
+    response = event_handler(event)
+    return response
