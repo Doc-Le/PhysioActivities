f84c9a16fee5420b483effa44b4f3734188ad49c
Doc-Le <76841050+Doc-Le@users.noreply.github.com>
2022-05-06 21:30:20 +0100

    Uncomment Path webhook in urls.py

--- bookings/urls.py
+++ bookings/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
+from .webhooks import webhook
 
 urlpatterns = [
     path('', views.bookings, name='bookings'),
     path('success/<booking_number>/', views.bookings_success, name='bookings_success'),
     path('cache_bookings_data/', views.cache_bookings_data, name='cache_bookings_data'),
-    # path('wh/', webhook, name='webhook'),
+    path('wh/', webhook, name='webhook'),
 ]
