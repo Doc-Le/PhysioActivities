20e220bf8f489c276ddaeb44449285fbb79eb3cf
Doc-Le <76841050+Doc-Le@users.noreply.github.com>
2022-05-09 19:17:22 +0100

    Create send_confirmation_email in webhook_handler.py

--- bookings/webhook_handler.py
+++ bookings/webhook_handler.py
@@ -1,16 +1,17 @@
- from django.http import HttpResponse
- from django.core.mail import send_mail
- from django.template.loader import render_to_string
- from django.conf import settings
+import datetime
+from django.http import HttpResponse
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.conf import settings
 
- from services.models import Service
+from services.models import Service, Clinician, ServiceDate, ServiceTime
 
- from .models import Booking
- from user.models import UserProfile
- from bookings.models import Booking
+from .models import Booking
+from user.models import UserProfile
+from bookings.models import Booking
 
- import json
- import time
+import json
+import time
 
 
 class StripeWH_Handler:
@@ -19,21 +20,23 @@ class StripeWH_Handler:
     def __init__(self, request):
         self.request = request
 
-    def _send_confirmation_email(self, Booking):
+    def _send_confirmation_email(self, booking):
         """Send the user a confirmation email"""
-        cust_email = UserProfile.email
-        subject = render_to_string(
-            'bookings/emails/confirmation_email_subject.txt',
-            {'Booking': Booking})
-        body = render_to_string(
-            'bookings/emails/confirmation_email_body.txt',
-            {'Booking': Booking, 'contact_email': settings.DEFAULT_FROM_EMAIL})
+
+        contact_email = settings.DEFAULT_FROM_EMAIL
+        subject_email_template = 'bookings/emails/confirmation_email_subject.txt'
+        body_email_template = 'bookings/emails/confirmation_email_body.txt'
+
+        user_email = UserProfile.email
+
+        subject = render_to_string(subject_email_template, {'booking': booking})
+        body = render_to_string(body_email_template, {'booking': booking, 'contact_email': contact_email})
 
         send_mail(
             subject,
             body,
-            settings.DEFAULT_FROM_EMAIL,
-            [cust_email]
+            contact_email,
+            [user_email]
         )
 
     def handle_event(self, event):
@@ -52,79 +55,65 @@ class StripeWH_Handler:
         pid = intent.id
         bag = intent.metadata.bag
         save_info = intent.metadata.save_info
+        username = intent.metadata.username
 
-        billing_details = intent.charges.data[0].billing_details
-        shipping_details = intent.shipping
-        grand_total = round(intent.charges.data[0].amount / 100, 2)
-
-        # Clean data in the shipping details
-        for field, value in shipping_details.address.items():
-            if value == "":
-                shipping_details.address[field] = None
+        user = UserProfile.objects.get(user__username=username)
+        service = Service.objects.get(id=bag['service'])
+        clinician = Clinician.objects.get(id=bag['clinician'])
+        date = ServiceDate.objects.get(id=bag['date'])
+        time = ServiceTime.objects.get(id=bag['time'])
+        date_time = datetime.strptime(bag['datetime'][:-1], '%d/%m/%Y %H:%M')
+        total = float(bag['total'])
 
         # Update profile information if save_info was checked
         profile = None
-        username = intent.metadata.username
-        if username != 'AnonymousUser':
-            profile = UserProfile.objects.get(user__username=username)
-            if save_info:
-                profile.default_phone_number = shipping_details.phone
-                profile.default_country = shipping_details.address.country
-                profile.default_postcode = shipping_details.address.postal_code
-                profile.default_town_or_city = shipping_details.address.city
-                profile.default_street_address1 = shipping_details.address.line1
-                profile.default_street_address2 = shipping_details.address.line2
-                profile.default_county = shipping_details.address.state
-                profile.save()
 
-        Booking_exists = False
+        booking_exists = False
         attempt = 1
         while attempt <= 5:
             try:
-                Booking = Booking.objects.get(
-                    full_name__iexact=shipping_details.name,
-                    email__iexact=billing_details.email,
-                    phone_number__iexact=shipping_details.phone,
-                    country__iexact=shipping_details.address.country,
-                    postcode__iexact=shipping_details.address.postal_code,
-                    town_or_city__iexact=shipping_details.address.city,
-                    street_address1__iexact=shipping_details.address.line1,
-                    street_address2__iexact=shipping_details.address.line2,
-                    county__iexact=shipping_details.address.state,
-                    grand_total=grand_total,
+                booking = Booking.objects.get(
+                    user=user,
+                    service=service,
+                    clinician=clinician,
+                    date=date,
+                    time=time,
+                    date_time=date_time,
+                    total=total,
                     original_bag=bag,
                     stripe_pid=pid,
                 )
-                Booking_exists = True
+                booking_exists = True
                 break
             except Booking.DoesNotExist:
                 attempt += 1
                 time.sleep(1)
-        if Booking_exists:
-            self._send_confirmation_email(Booking)
+        if booking_exists:
+            self._send_confirmation_email(booking)
             return HttpResponse(
                 content=(f'Webhook received: {event["type"]} | SUCCESS: '
                          'Verified Booking already in database'),
                 status=200)
         else:
-            Booking = None
+            booking = None
             try:
-                Booking = Booking.objects.create(
-                    user={}, # get user
-                    service={}, # get service
-                    clinician={}, # get clinician
-                    date={}, # get date,
-                    time={}, # get date,
-                    total=0.0,
+                booking = Booking.objects.create(
+                    user=user,
+                    service=service,
+                    clinician=clinician,
+                    date=date,
+                    time=time,
+                    date_time=date_time,
+                    total=total,
                     stripe_pid=pid,
                 )
             except Exception as e:
-                if Booking:
-                    Booking.delete()
+                if booking:
+                    booking.delete()
                 return HttpResponse(
                     content=f'Webhook received: {event["type"]} | ERROR: {e}',
                     status=500)
-        self._send_confirmation_email(Booking)
+        self._send_confirmation_email(booking)
         return HttpResponse(
             content=(f'Webhook received: {event["type"]} | SUCCESS: '
                      'Created Booking in webhook'),
