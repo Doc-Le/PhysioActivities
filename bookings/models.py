374739aab6f037b14c96d3d898baafb6612c08ab
Doc-Le <76841050+Doc-Le@users.noreply.github.com>
2022-05-09 17:04:15 +0100

    Replace user foreighKey in models.py

--- bookings/models.py
+++ bookings/models.py
@@ -2,12 +2,12 @@ from statistics import mode
 import uuid
 from django.db import models
 from services.models import Clinician, Service, ServiceDate, ServiceTime
-from user.models import User
+from user.models import UserProfile
 
 
 class Booking(models.Model):
     booking_number = models.CharField(max_length=32, null=False, editable=False)
-    user = models.ForeignKey(User, null=False, blank=False, on_delete=models.CASCADE)
+    user = models.ForeignKey(UserProfile, null=False, blank=False, on_delete=models.CASCADE)
     service = models.ForeignKey(Service, null=False, blank=False, on_delete=models.CASCADE)
     clinician = models.ForeignKey(Clinician, null=False, blank=False, on_delete=models.CASCADE)
     date = models.ForeignKey(ServiceDate, null=False, blank=False, on_delete=models.CASCADE)
