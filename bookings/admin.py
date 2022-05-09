b850acb9b011129fe8bc362d963cdddb24d8a19b
Doc-Le <76841050+Doc-Le@users.noreply.github.com>
2022-05-09 23:07:12 +0100

    Add ded patient in admin.py

--- bookings/admin.py
+++ bookings/admin.py
@@ -4,12 +4,15 @@ from .models import Booking
 
 class BookingAdmin(admin.ModelAdmin):
     model = Booking
+    
+    def patient(self, obj: Booking) -> str:
+        return obj.user.fullname()
 
     fields = ('user', 'service', 'clinician', 'date', 'time', 'total',)
 
     readonly_fields = ('datetime', 'total',)
 
-    list_display = ('datetime', 'booking_number', 'user', 'service', 'clinician', 'total',)
+    list_display = ('datetime', 'booking_number', 'patient', 'service', 'clinician', 'total',)
 
     ordering = ('-datetime',)
 
